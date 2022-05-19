7fe463caff9e6b549e61b492ff47f4502a97701d
suemor <3100825062@qq.com>
2022-05-19 14:57:24 +0800

    feat: fontawesome icon

--- components/sidebar/index.vue
+++ components/sidebar/index.vue
@@ -1,35 +1,56 @@
 <template>
   <div class="side w-72 bg-white h-screen relative">
-    <div class="side-overlay"/>
+    <div class="side-overlay" />
     <div class="side-header">
       <a href="/" class="side-header-avatar">
         <img src="https://suemor.oss-cn-beijing.aliyuncs.com/img/avatar.jpeg">
       </a>
-      <div class="text-3xl font-serif mt-6 transition ">
-        <a href="/" class="text-gray-700 hover:text-gray-500">suemor</a>
+      <div class="text-3xl font-serif mt-6 transition-all">
+        <a href="/" class="text-gray-700 transition hover:text-gray-500">suemor</a>
       </div>
-        <ul class="text-md text-gray-500 p-7">
-          <li v-for="item in pageTitle" :key="item" class="mt-2">
-              {{item}}
-          </li>
-        </ul>
+      <ul class="text-md text-gray-500 p-7">
+        <li v-for="item in pageTitle" :key="item" class="mt-2 cursor-pointer transition-all hover:text-gray-400">
+          {{ item }}
+        </li>
+      </ul>
 
+      <div class="flex justify-around w-3/5 m-auto">
+        <i v-for="icon in icons" :class="icon.icon" :style="{color:icon.color}"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang='ts'>
 
-const pageTitle:string[] = ['主页','归档','标签','关于','友链']
-const icon = [
+const pageTitle: string[] = ['主页', '归档', '标签', '关于', '友链']
+const icons = [
   {
-    name:'github',
-    icon:'',
-    url:''
+    name: 'github',
+    icon: 'fa-brands fa-github',
+    url: 'https://github.com/suemor233',
+    color:'#000'
+  },
+  {
+    name: 'twitter',
+    icon: 'fa-brands fa-twitter',
+    url: 'https://twitter.com/Suemor233',
+    color: '#4AA1E7'
+  },
+  {
+    name: 'qq',
+    icon: 'fa-brands fa-qq',
+    url: 'https://wpa.qq.com/msgrd?v=3&uin=1502972236&site=qq&menu=yes',
+    color: '#55B4EF'
+  },
+  {
+    name: 'bilibili',
+    icon: 'fa-brands fa-bilibili',
+    url: 'https://github.com/suemor233',
+    color: '#479FD1'
   }
 ]
 </script>
-
 <style lang="scss" scoped>
 .side {
   &-overlay {
@@ -47,6 +68,7 @@ const icon = [
       position: relative;
       display: flex;
       justify-content: center;
+
       img {
         opacity: 1;
         height: 8em;
@@ -58,5 +80,4 @@ const icon = [
   }
 
 }
-
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
-import { defineNuxtConfig } from 'nuxt'
+import {defineNuxtConfig} from 'nuxt'
 import {resolve} from "path";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    css: ["~/assets/css/tailwind.css"],
-    build: {
-        transpile: ['vueuc'],   // fix dev error: Cannot find module 'vueuc'
+    meta: {
+        link: [
+          {
+            href: 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/all.min.css',
+            rel: 'stylesheet',
+          }
+        ],
+    },
+    css: ["~/assets/css/tailwind.css",'@fortawesome/fontawesome-svg-core/styles.css'],
+    build: { // fix dev error: Cannot find module 'vueuc'
         postcss: {
             postcssOptions: {
                 plugins: {
